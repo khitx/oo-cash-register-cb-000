e7da15656b23174eb2ae798d1e1d6f04aa3cb831
Kathy Hudson <khitx@icloud.com>
2018-01-28 22:22:41 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,10 +14,14 @@ class CashRegister
     end
 
     def discount
-      @discount = 20
+      @discount = 0.20
     end
 
     def add_item(item, price, quantity = 1)
       @total += (price * quantity)
+    end
+
+    def apply_discount
+      @total *= @discount
     end
 end
