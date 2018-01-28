cf53625abc8a2f6585d24b34c069cbf88614c071
Kathy Hudson <khitx@icloud.com>
2018-01-28 22:56:18 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,7 @@ class CashRegister
     end
 
     def apply_discount
-      if @discount > 0 
+      if @discount > 0
         @total *= (1 - @discount.fdiv(100))
         "After the discount, the total comes to $#{@total.floor}."
       else
